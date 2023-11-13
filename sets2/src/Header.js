768c9ead8a1817932d19ae3ba4bb4fb412654a4e
Caroline Peake <peake.caroline@gmail.com>
2023-11-13 14:21:09 -0800

    delete commented out Difficulty dropdown from Header component

--- sets2/src/Header.js
+++ sets2/src/Header.js
@@ -34,32 +34,13 @@ export default function Header({ gameStatus, setGameStatus }: HeaderProps) {
 
     const RightButtonText = gameStatus === 'paused' ? 'Resume Game' : gameStatus === 'started' || gameStatus === 'resumed' ? 'Pause Game' : '';
 
-  const [expand, setExpand] = useState(false);
 
   return (
     <header className="App-header">
       <h1 className="header-logo">SET</h1>
       <div className="header-links">
         <a className="header-link" href="https://www.wikihow.com/Play-SET" target="_blank" rel="noreferrer">tutorial</a>
-        {/* <span className="header-link">games</span> */}
-        <div className="header-link header-dropdown" onMouseEnter={() => setExpand(true)} onMouseLeave={() => setExpand(false)}>
-          <span>
-            difficulty &#9662;
-          </span>
-          {expand &&
-            <Dropdown>
-            <div>
-              Easy
-            </div>
-            <div>
-              Medium
-            </div>
-            <div>
-              Hard
-            </div>
-          </Dropdown>}
-        </div>
-
+        <span className="header-link">games</span>
         <span
           className="header-link header-button"
           onClick={handleStartEndGame}
@@ -126,12 +107,6 @@ const StyledButton = styled(Button)`
         `
     };
   }}
-`;
-
-const Dropdown = styled.div`
-  position: absolute;
-  display: flex;
-  flex-direction: column;
 `;
 
 
