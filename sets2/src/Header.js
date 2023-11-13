772011af2d370b9404227e7f13be813e593a64f7
Caroline Peake <peake.caroline@gmail.com>
2023-11-13 15:38:29 -0800

    move Difficulty dropdown into header, delete all commented out code in Header component, change start/end game button to quit game all the time

--- sets2/src/Header.js
+++ sets2/src/Header.js
@@ -1,28 +1,25 @@
-import React, { useState } from 'react';
-import { Timer } from './Timer';
-import styled, { css } from 'styled-components';
+import React from 'react';
 import './index.css';
 import './App.css';
-import { Button } from './StyledComponents';
+import Difficulty from './Difficulty';
 
 type HeaderProps = {
   gameStatus: 'idle' | 'started' | 'paused' | 'resumed' | 'ended';
   setGameStatus: React.Dispatch<React.SetStateAction<string>>;
+  difficulty: 'Easy' | 'Medium' | 'Hard';
+  setDifficulty: React.Dispatch<React.SetStateAction<string>>;
 };
 
-export default function Header({ gameStatus, setGameStatus }: HeaderProps) {
+export default function Header({
+  gameStatus,
+  setGameStatus,
+  difficulty,
+  setDifficulty,
+}: HeaderProps) {
 
   // TO-DO: have floating deal button and then after facedown cards are dealt have floating start game button slide in from the top
     // have the button have a box-shadow and get bigger and smaller over a period of seconds to look as if it's floating
     // might also want to put a glow behind it
-
-    const handleStartEndGame = () => {
-      if (gameStatus === 'idle' || gameStatus === 'ended') {
-        setGameStatus('started');
-      } else {
-        setGameStatus('ended');
-      }
-    };
 
     const handleEndGame = () => {
       if (gameStatus !== 'ended') {
@@ -30,91 +27,30 @@ export default function Header({ gameStatus, setGameStatus }: HeaderProps) {
       }
     };
 
-    const handlePauseGame = () => {
-      gameStatus === 'paused' ? setGameStatus('resumed') : setGameStatus('paused');
-    };
-
-    // const LeftButtonText = gameStatus === 'ended' || gameStatus === 'idle' ? 'Start Game' : 'End Game';
-
-    const LeftButtonText = gameStatus === 'ended' || gameStatus === 'idle' ? 'start' : 'quit';
-
-    const RightButtonText = gameStatus === 'paused' ? 'Resume Game' : gameStatus === 'started' || gameStatus === 'resumed' ? 'Pause Game' : '';
-
-
   return (
     <header className="App-header">
       <h1 className="header-logo">SET</h1>
       <div className="header-links">
         <a className="header-link" href="https://www.wikihow.com/Play-SET" target="_blank" rel="noreferrer">tutorial</a>
-        <span className="header-link">games</span>
+        {/* <span className="header-link">games</span> */}
+        <Difficulty
+          difficulty={difficulty}
+          setDifficulty={setDifficulty}
+        />
         <span
           className="header-link header-button"
-          onClick={handleStartEndGame}
           onClick={handleEndGame}
-          // style={{display: gameStatus === 'ended' ? 'none' : 'flex'}}
         >
-          {/* exit */}
           quit
-          {/* {LeftButtonText} */}
         </span>
       </div>
-      {/* <HeaderLeft className="header-item">
-        <StyledButton
-        $color="dark"
-        onClick={handleStartEndGame}
-        style={{display: gameStatus === 'ended' ? 'none' : 'flex'}}
-        >
-          {LeftButtonText}
-        </StyledButton>
-      </HeaderLeft>
-
-      <HeaderRight className="header-item">
-        <StyledButton $color="light" onClick={handlePauseGame} style={{display: gameStatus ===  'idle' || gameStatus === 'ended' ? 'none' : 'flex'}}>{RightButtonText}</StyledButton>
-        <Timer
-        gameStatus={gameStatus}
-        />
-      </HeaderRight> */}
     </header>
   );
 };
 
 
 
-const HeaderLeft = styled.div`
-  justify-content: left;
-  margin-left: 2%;
-`;
 
-const StyledButton = styled(Button)`
-  ${(props) => {
-    switch (props.$color) {
-      case "dark":
-        return css`
-          background-color: var(--darkBtnColor);
-          color: white;
-          &:hover {
-            background-color:
-          }
-        `;
-      case "light":
-        return css`
-          background-color: var(--yellow);
-          background-color: var(--lightBtnColor);
-          color: white;
-          &:hover {
-            background-color:
-          }
-        `;
-      default:
-        return css`
-          background-color: transparent;
-          color: black;
-          &:hover {
-            background-color: gray;
-          }
-        `
-    };
-  }}
-`;
+
 
 
