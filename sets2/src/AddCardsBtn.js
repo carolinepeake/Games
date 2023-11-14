93e87b06b3ef8e2ef715681e5b596a65bc737c83
Caroline Peake <peake.caroline@gmail.com>
2023-11-13 16:02:55 -0800

    disable button when there are 15 cards showing and before a set amount of time has passed each round

--- sets2/src/AddCardsBtn.js
+++ sets2/src/AddCardsBtn.js
@@ -1,27 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Button } from './StyledComponents.js';
 
-export default function AddCardsBtn({ deck }) {
+export default function AddCardsBtn({ deck, setExtraCards, extraCards }) {
 
   const [active, setActive] = useState(false);
 
   useEffect(() => {
     setActive(false);
-    const timeout = (() => setActive(true), 20000);
+    const timeout = setTimeout(() => {setActive(true)}, 10000);
     return () => clearTimeout(timeout);
   }, [deck]);
 
+  const handleAddCards = () => {
+    setExtraCards(true)
+  };
+
 
   return (
-    <StyledButton $disabled={!active}>
+    <StyledButton $disabled={!active || extraCards} disabled={!active || extraCards} onClick={handleAddCards}>
       Add 3 More Cards
     </StyledButton>
   );
 }
 
 const StyledButton = styled(Button)`
-  ${props => props.$disabled && 'opacity: 50%'};
+  margin-top: 1rem;
+  ${props => !props.$disabled && 'color: blue'};
   box-shadow: none;
+  &:hover {
+    box-shadow: none;
+    ${props => !props.$disabled && css`
+      text-decoration: underline;
+      font-weight: bold;
+    `};
+  }
 
 `;
