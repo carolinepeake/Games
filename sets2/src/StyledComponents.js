2e60ef49dad5f6ec83a9bd1aca7c4d831d0d1171
Caroline Peake <peake.caroline@gmail.com>
2024-01-29 22:31:46 -0800

    remove secondary button styles

--- sets2/src/StyledComponents.js
+++ sets2/src/StyledComponents.js
@@ -6,26 +6,32 @@ export const Button=styled.button`
   display: inline-flex;
   align-items: center;
   justify-content: center;
-  background-color: ${props => props.theme};
-  color: #fff;
+  text-align: center;
   font-size: clamp(12px, 2vh, 24px);
+  line-height: 1;
+  min-width: 10ch;
   font-weight: bold;
   border-radius: 15px;
-  padding: 0.75em 1.5em;
+  padding: 0.5em 0.75em;
   box-shadow: 0 3px 5px rgba(0, 0, 0, 0.18);
-  min-width: 10ch;
-  text-align: center;
-  line-height: 1;
   &:hover {
-    background-color: scale-color(var(--darkBtnColor), var(--oldBtnColor): -20%);
+   /* background-color: scale-color(var(--darkBtnColor), --lightness: -20%); */
+  /*  background-color: scale-color(var(--darkBtnColor), var(--oldBtnColor): -20%);
+    background-color: var(--lightBtnColor); */
     box-shadow: 5px 5px 10px 1px rgba(63, 61, 61, 0.79);
   }
   ${props => props.as === 'a' && css`
     text-decoration: none;
   `}
-`
-// &:hover {
-//   background-color: scale-color(var(--darkBtnColor), --lightness: -20%);
+`;
+
+// .btn-primary {
+//   background-color: ${props => props.theme.darkBtnColor};
+//   color: #fff;
 // }
- /* min-height: 44px; 44pxx44px is the recommendation target size for accessibility */
+
+// .btn-secondary {
+//   color: ${props => props.theme.lightBtnColor};
+//   border: 1px ${props => props.theme.lightBtnColor} solid;
+// }
 
