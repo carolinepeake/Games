af7762c326abc0f78ccbd70ae85f1064e305de76
Caroline Peake <peake.caroline@gmail.com>
2024-02-08 05:29:23 -0800

    add action types START, QUIT, PAUSE, RESUME to gameReducer; refactor SELECT_DIFFICULTY, ADD_CARDS, SELECT_CARD, CLICK_SET to return state for failed edge cases, instead of returning newState, so doesn't cause a rerender; refactor SELECT_CARD to assign isSet state variable to result of calling checkSet, either calling setFound or setNotFound depending on value of isSet, move setTimeout function resetting initial state values after showing module stating whether a set was found or not for a set amount of time inside SELECT-CARD method, include checkforWin function call, delete CONTINUE and CLEAR action types and move the state changes to SELECT_CARD so all state changes are kept inside function triggered by user action and not called by a different action type function; comment out COUNTDOWN function and will move inside CLICK_SET function for same reason as CONTINUE/CLEAR deletion (TODO: move inside CLICK_SET function); add DIFFICULTY_VALUES object to provide values for dropdown label and timeout milliseconds for each difficulty state value; added logging an error to the console as default behavior, in other words when the action type is not one of the stated action types in the relevant reducer; rename some gameState variables; add argument values to test getWinner function; add  getWinner function to determine the winner[s] name[s] from the players state object, which includes player objects keyed to playerIds, with the setCnt and name properties; add checkForWin function (TODO: write block checking if any set possible to make from the remaining cards (deck)); refactor checkSet function by  separating out checkFeature function

--- sets2/src/State/gameReducer.js
+++ sets2/src/State/gameReducer.js
@@ -6,18 +6,136 @@ import { checkSet, getSet, replaceCards } from '../utils/gamePlay';
 // TODO: add TIMEOUT action
 // TODO: add ADD_CARDS action and maybe board size state
 
+const DIFFICULTY_VALUES = {
+  '1': {
+    speed: 30000,
+    label: 'Easy',
+  },
+  '2': {
+    speed: 20000,
+    label: 'Hard',
+  },
+  '3': {
+    speed: 10000,
+    label: 'Impossible',
+  },
+};
+
+function checkFeature(selectedCards, feature) {
+  const featureValues = selectedCards.map(card => card[feature]);
+  const uniqueValues = [...new Set(featureValues)];
+  return !(uniqueValues.length === 2);
+};
+
+function checkSet(selectedCards) {
+  if (!checkFeature(set, 'color')) {
+    return false;
+  }
+  if (!checkFeature(set, 'shading')) {
+    return false;
+  }
+  if (!checkFeature(set, 'shape')) {
+    return false;
+  }
+  if (!checkFeature(set, 'count')) {
+    return false;
+  }
+
+  return true;
+  // return (checkFeature(set, 'color') && checkFeature(set, 'count') && checkFeature(set, 'shape') && checkFeature(set, 'shading'))
+}
+
+function checkForWin(deck) {
+  if (deck.length === 0) {
+    return true;
+  }
+  if (deck.length >= 21 || deck.length === 3) {
+    return false;
+  }
+  // check if no set in remaining cards in deck
+  return;
+}
+
+// display winner text only if game completed; otherwise display playersboard only
+function getWinner(players) {
+  let winners = [];
+  // let lrgstSetCnt = 0;
+  let totalSetCnt = 0;
+    for (const player in players) {
+      const ([id] : { setCnt, name } = player);
+      const { setCnt2, name2 } = player;
+      totalSetCnt += setCnt;
+      let lrgstSetCnt = players[winners[0]].score;
+      console.log('setCnt: ', setCnt, 'setCnt2: ', setCnt2, 'name: ', name, 'name2: ', name2, 'id: ', id, 'player: ', player, 'players: ', players, 'totalSetCnt :', totalSetCnt);
+      if (setCnt > lrgstSetCnt) {
+        winners = [id];
+        // lrgstSetCnt = setCnt;
+      } else if (setCnt === lrgstSetCnt) {
+        winners.push(id);
+      }
+    }
+    // if !(21 <= totalSetCnt && totalSetCnt >= 27) // learn how to write sysinctly
+    if (totalSetCnt < 21 || totalSetCnt > 27) {
+      console.error("impossible total sets count");
+      return;
+    }
+
+    if (winners.length === 1) {
+      const winner = winners[0];
+      return `${players[winner].name} wins!`;
+    } else {
+      return 'Tie!';
+    }
+  };
+
+  // Test
+  const winnerText = getWinner({
+    '01': {
+      name: 'player1',
+      setCnt: 2,
+    },
+    '02': {
+      name: 'player2',
+      setCnt: 20,
+    },
+    '03': {
+      name: 'player3',
+      setCnt: 2,
+    },
+  });
+  console.log('winnerText', winnerText);
+
+
 const clone = x => JSON.parse(JSON.stringify(x));
 
 export const getInitialState = () => ({
-  status: 'idle',
+  status: 'idle', // starting, inPlay, paused, ended
+    // idle, start, pause, resume, end
   deck: getNewDeck(),
   board: [],
   cardsShowing: 12,
+  // boardSize: 12 // don't need both boardSize and board b/c can just grow board when 3 more cards button clicked
   selectedCards: [],
-  set: null,
-  turn: null,
-  score: [0, 0],
-  difficulty: 'easy',
+  // set: [],
+  isSet: null, // null, false, true => module state isVisible && text
+  // turn: null,
+  activePlayer = '', // '', 'player1' || '0', 'player2' || '01'
+  // score: [0, 0],
+  players: {
+    '01': {
+      name: 'player1',
+      setCnt: 0,
+    },
+    '02': {
+      name: 'bot',
+      setCnt: 0,
+    },
+    // '03': {
+    //   name: 'player3',
+    //   setCnt: 0,
+    // },
+  },
+  difficulty: '1',
   timeRemaining: 10,
 });
 
@@ -29,85 +147,162 @@ export const gameReducer = (state, action) => {
       return newState;
     }
 
-    case 'CLICK_SET': {
+    case 'QUIT': {
       const newState = clone(state);
-      newState.turn = action.payload;
+      newState.status = 'ended';
+      return newState;
+    }
+
+    // case 'GAME_OVER': {
+    //   const newState = clone(state);
+    //   newState.status = 'ended';
+    //   return newState;
+    // }
+
+    case 'PAUSE': {
+      const newState = clone(state);
+      newState.status = 'pause';
+      return newState;
+    }
+
+    case 'RESUME': {
+      const newState = clone(state);
+      newState.status = 'inPlay';
+      return newState;
+    }
+
+    // case 'PLAY': {
+    //   const newState = clone(state);
+    //   newState.status = 'inPlay';
+    //   return newState;
+    // }
+
+    case 'CLICK_SET': {
+      if (state.activePlayer) {
+        return state;
+      }
+      const newState = clone(state);
+      // newState.turn = action.payload;
+      newState.activePlayer = action.payload;
       return newState;
     }
 
     case 'SELECT_CARD': {
       const newState = clone(state);
-      const { selectedCards, turn } = newState;
-
-      // if player didn't click set or card already selected do nothing
+      // const { selectedCards, turn } = newState;
+      const { deck, selectedCards, activePlayer, players, status } = newState;
       const { card, player } = action.payload;
-
-      if (player !== turn) {
+      // if (player !== turn) {
+      //   return state;
+      // }
+      if (player !== activePlayer) {
         return state;
       }
       if (selectedCards.map(selectedCard => selectedCard.id).includes(card.id)) {
         return state;
       }
-
-      // if not selected, update selectedCards state
-      selectedCards.push(action.payload);
-
-      // if 3 cards selected, check if set
-      if (selectedCards.length === 3) {
-        newState.set = checkSet(selectedCards);
-        // TODO: update score
-      }
-
+      selectedCards.push(card);
       newState.selectedCards = selectedCards;
+
+      if (selectedCards.length === 3) {
+        const isSet = checkSet(selectedCards);
+        newState.isSet = isSet;
+        if (isSet) {
+          setFound();
+            // TODO: update players
+            const newDeck = replaceCards(deck, selectedCards);
+            newState.deck = newDeck;
+            newState.cardsShowing = Math.min(12, newDeck.length);
+        } else {
+          // alert(isSet[1]);
+          setNotFound();
+        }
+        setTimeout(() => {
+          // this.currentSet[0].bg = this.color2;
+          // this.currentSet[1].bg = this.color2;
+          // this.currentSet[2].bg = this.color2;
+          // this.setState({ clicked: 0 });
+          // const isSet = checkSet(selectedCards);
+          newState.isSet = null;
+          // if (isSet) {
+          //   setFound();
+          // } else {
+          //   alert(isSet[1]);
+          // }
+          // newState.isSet = isSet;
+          newState.selectedCards = [];
+         // newState.set = [];
+         newState.timeRemaining = 10;
+         newState.turn = null;
+         newState.activePlayer = '';
+          // newState.currentSet = [];
+          if (checkForWin(deck)) {
+            const scoreboardText = getWinner(players);
+            console.log('scoreboard: ', scoreboardText);
+            newState.status = 'ended';
+          }
+          console.log('timeout newState: ', newState);
+          return newState;
+        }, 500);
+      }
+      console.log('newState: ', newState);
       return newState;
     }
 
-    case 'CONTINUE': {
-      const newState = clone(state);
-      const { deck, selectedCards } = newState;
+    // case 'CONTINUE': {
+    //   const newState = clone(state);
+    //   const { deck, selectedCards } = newState;
+    //   const newDeck = replaceCards(deck, selectedCards);
+    //   newState.deck = newDeck;
+    //   newState.cardsShowing = Math.min(12, newDeck.length);
+    //   newState.isSet = null;
+    //   // newState.isSet = false;
+    //   newState.selectedCards = [];
+    //   // newState.set = [];
+    //   newState.timeRemaining = 10;
+    //   newState.turn = null;
+    //   return newState;
+    // }
 
-      const newDeck = replaceCards(deck, selectedCards);
+    // case 'CLEAR': {
+    //   const newState = clone(state);
+    //   newState.isSet = null;
+    //   // newState.isSet = false;
+    //   newState.selectedCards = [];
+    //   // newState.set = [];
+    //   newState.timeRemaining = 10;
+    //   return newState;
+    // }
 
-      newState.deck = newDeck;
-      newState.cardsShowing = 12;
-      newState.set = null;
-      newState.selectedCards = [];
-      newState.timeRemaining = 10;
-      newState.turn = null;
-
-      return newState
-    }
-
-    case 'CLEAR': {
-      const newState = clone(state);
-      newState.set = null;
-      newState.selectedCards = [];
-      newState.timeRemaining = 10;
-      return newState
-    }
-
-    case 'COUNTDOWN': {
-      const newState = clone(state);
-      const { timeRemaining } = newState;
-      newState.timeRemaining = timeRemaining - 1;
-      return newState;
-    }
+    // case 'COUNTDOWN': {
+    //   const newState = clone(state);
+    //   const { timeRemaining } = newState;
+    //   newState.timeRemaining = timeRemaining - 1;
+    //   return newState;
+    // }
 
     case 'ADD_CARDS': {
+      if (state.deck.length < 15) {
+        return state;
+      }
       const newState = clone(state);
       newState.cardsShowing = 15;
       return newState;
+
     }
 
     case 'SELECT_DIFFICULTY': {
-
+      if (state.difficulty === action.payload) {
+        return state;
+      }
       const newState = clone(state);
       newState.difficulty = action.payload;
-
+      // DIFFICULTY_VALUES = obj providing milliseconds and dropdown label values for state values
       return newState;
     }
 
     default:
+      console.error(action.type, "with a ", action.payload, "payload, is an unknown action type in gameReducer");
       return state
   }
 };
