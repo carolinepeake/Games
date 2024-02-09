3561425a3b564a43b5bd761221633a3216da9c57
Caroline Peake <peake.caroline@gmail.com>
2024-02-09 02:08:10 -0800

    remove difficulty constant from gameReducer

---
+++ sets2/src/State/gameConstants.js
@@ -0,0 +1,14 @@
+export const DIFFICULTY_VALUES = {
+  '1': {
+    speed: 30000,
+    label: 'Easy',
+  },
+  '2': {
+    speed: 20000,
+    label: 'Hard',
+  },
+  '3': {
+    speed: 10000,
+    label: 'Impossible',
+  },
+};
