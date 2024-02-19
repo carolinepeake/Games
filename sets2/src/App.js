7fc2fa4fb3fae49f2704a67fa9c04a492a695c9f
Caroline Peake <peake.caroline@gmail.com>
2024-02-18 23:12:03 -0800

    delete conditional logic rendering either board or play again button in favor of moving logic to Board component, possibly in the form of conditional XML returns

--- sets2/src/App.js
+++ sets2/src/App.js
@@ -1,76 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
+import React, { useReducer } from 'react';
+// import styled from 'styled-components';
 import { Board } from './Board/Board.js';
-import { Board2 } from './Board/Board2.js';
 import Header from './Header';
 import './App.css';
 import './index.css';
-import { Button } from './StyledComponents';
+// import { Button } from './StyledComponents';
+import { gameReducer, getInitialState } from './State/gameReducer';
 
 // TODO: need to memoize Board or move props that are changing b/c Board is getting rerendered every time a move is made
 
 const App = () => {
 
-  // const restartGame = () => {
-  //   setGameStatus('idle');
-  //   setDeck(shuffleDeck(unshuffledDeck));
-  //   // setSelectedCards([]);
-  //   // setActivePlayer('');
-  //   // setExtraCards(false);
-  //   // setP1Score(0);
-  //   // setP2Score(0);
-  // };
+  const [state, dispatch] = useReducer(gameReducer, getInitialState());
+
+  function handleSelectDifficulty(level = '1') {
+    dispatch({type: 'SELECT_DIFFICULTY', payload: level});
+  }
 
   return (
     <div className="App">
-      {/* <Header
-        setGameStatus={setGameStatus}
-        gameStatus={gameStatus}
-        difficulty={difficulty}
-        setDifficulty={setDifficulty}
-        setDeck={setDeck}
-      /> */}
-
+      <Header
+          handleSelectDifficulty={handleSelectDifficulty}
+          difficulty={state.difficulty}
+      />
       <div className="App-main">
-        {/* {gameStatus === 'ended'
-        ? <StyledButton
-            type="button"
-            onClick={restartGame}
-          >
-            Play Again!
-          </StyledButton>
-        : <Board
-            gameStatus={gameStatus}
-            setGameStatus={setGameStatus}
-            difficulty={difficulty}
-            setDifficulty={setDifficulty}
-            deck={deck}
-            setDeck={setDeck}
-          />} */}
-
-          <Board2/>
-
-{/* {gameStatus === 'ended'
-        && <StyledButton
-            type="button"
-            onClick={restartGame}
-          >
-            Play Again!
-          </StyledButton>}
-
-
-           */}
-
+          <Board
+            status={state.status}
+            difficulty={state.difficulty}
+            deck={state.deck}
+          />
       </div>
-
     </div>
   );
 }
 
 export default App;
 
-const StyledButton = styled(Button)`
-  background-color: var(--darkBtnColor);
-  font-size: 32px;
-  display: inline-block;ß
-`;
+// const StyledButton = styled(Button)`
+//   background-color: var(--darkBtnColor);
+//   font-size: 32px;
+//   display: inline-block;ß
+// `;
