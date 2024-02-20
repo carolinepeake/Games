6d3a50c075f9b5a9a5685aa87833280f8737be1c
Caroline Peake <peake.caroline@gmail.com>
2024-02-20 07:16:22 -0800

    move Pause Button into Timer component, delete container  component TopControls and render Timer component and div element directly from Board.js; replace container div styled component TopControls with className flexbox top-controls

--- sets2/src/Timer.js
+++ sets2/src/Timer.js
@@ -1,31 +1,53 @@
 import React, { useState, useEffect } from 'react';
 
+// import './index.css';
+// import './App.css';
+import './Timer.css';
+
 type TimerProps = {
-  gameStatus: 'idle' | 'started' | 'paused' | 'resumed' | 'ended';
+  status: 'idle' | 'started' | 'paused' | 'resumed' | 'ended';
+  // handleClickPause:
 };
 
-export const Timer = ({ gameStatus }: TimerProps) => {
+export const Timer = ({
+  status,
+  handleClickPause
+ }: TimerProps) => {
 
   const [elapsedSecs, setElapsedSecs] = useState(0);
 
   useEffect(() => {
     let interval;
-    if (gameStatus === 'started' || gameStatus === 'resumed') {
+
+    if (status === 'started' || status === 'resumed') {
       interval = setInterval(() => {
           setElapsedSecs(prevSecs => prevSecs + 1);
       }, 1000);
-    } else if (gameStatus === 'idle') {
+
+    } else if (status === 'idle') {
       setElapsedSecs(0)
     }
+
     return () => clearInterval(interval);
-  }, [gameStatus]);
+
+  }, [status]);
 
   let timerText = Math.floor(elapsedSecs / 60) + ':' + ('0' + (elapsedSecs % 60)).slice(-2);
   if (elapsedSecs >= 3600) {
     timerText = Math.floor(elapsedSecs / 3600) + ':' + timerText;
   };
 
+  const buttonText = status === 'paused' ? 'Resume' : status === 'started' || status === 'resumed' ? 'Pause' : '';
+
   return (
-    <div className="timer">{timerText}</div>
+    <div className="timer">
+      <span>{timerText}</span>
+      <button
+      className="btn pause"
+      onClick={handleClickPause}
+    >
+      {buttonText}
+    </button>
+  </div>
   );
 };
